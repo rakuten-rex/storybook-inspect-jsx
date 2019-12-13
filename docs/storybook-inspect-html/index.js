b68104d7a33926d773417a91596bb4192f5880eb
Claudio <claudio.moya@rakuten.com>
2019-12-13 11:28:56 +0900

    Added - Docs for github pages and README file

--- docs/storybook-inspect-html/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./storybook-inspect-html.production.min.js');
-} else {
-  module.exports = require('./storybook-inspect-html.development.js');
-}
